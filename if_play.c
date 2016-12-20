d0c0fe5fd1a8e755eab2d2bed5b1a5fb4bf0dbcb
KING, JAN ERWIN S <janerwinking.jek@gmail.com>
2016-12-20 08:00:24 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int value;
+	
+	printf("Enter a Value");
+	scanf("%d", &value);
+	if(value%3==0);
+	    {
+	    	printf("ED");
+		}
+	
+	if (value%5==0);
+		{
+	    printf("POGI");
+	
+		}
+		
+	
+	
+}
