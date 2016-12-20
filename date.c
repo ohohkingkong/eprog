5bc5a8ecb5c91687391da19e4992f799ebe9a21b
KING, JAN ERWIN S <janerwinking.jek@gmail.com>
2016-12-20 09:19:20 +0800

    Create date.c

---
+++ date.c
@@ -0,0 +1,69 @@
+
+int main(){
+	int X,Y;
+	
+		printf("SPECIFY A MONTH:");
+		scanf("%d", &X);
+		printf("SPECIFY A DAY:");
+		scanf("%d", &Y);
+		if (X==1){
+			printf("JANUARY");
+ 		}
+ 		if (X==2){
+			printf("FEBUARY");
+ 		}
+ 		if (X==3){
+			printf("MARCH");
+ 		}
+ 		if (X==4){
+			printf("APRIL");
+ 		}
+ 		if (X==5){
+			printf("MAY");
+ 		}
+ 		if (X==6){
+			printf("June");
+ 		}
+ 		if (X==7){
+			printf("JULY");
+ 		}
+		if (X==8){
+			printf("AUGUST");
+ 		}
+ 		if (X==9){
+			printf("SEPTEMBER");
+ 		}
+ 		if (X==10){
+			printf("OCTOBER");
+ 		}
+ 		if (X==11){
+			printf("NOVEMBER");
+ 		}
+ 		if (X==12){
+			printf("DECEMBER");
+ 		}
+ 		else if (X>=13){
+		 }
+ 			printf("12 months a year");
+ 		if (Y==1){
+ 			printf("Sunday");
+		}
+		if (Y==2){
+ 			printf("Monday");
+		}
+		if (Y==3){
+ 			printf("Tuesday");
+		}
+		if (Y==4){
+ 			printf("Wednesday");
+		}
+		if (Y==5){
+ 			printf("Thursday");
+		}
+		if (Y==6){
+ 			printf("Friday");
+		}
+		if (Y==7){
+ 			printf("Saturday");
+		}
+		else if (Y>=8){
