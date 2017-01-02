21780e5c076ed25a78586cddb317253d200b0e96
KING, JAN ERWIN S <janerwinking.jek@gmail.com>
2017-01-03 07:46:56 +0800

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+
+int main(){
+    int rem,i;
+        printf("\n The odd numbers between 1 and 100 are \n"); 
+        for(i=1; i<=100; ++i){
+        rem = i % 2;
+        if(rem != 0)
+        printf("\n  %d",i);
+               }
+    return 0; 
+    
+    } 
