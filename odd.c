416b6fbba91c69d5b6f825fef6262b99d098f478
KING, JAN ERWIN S <janerwinking.jek@gmail.com>
2017-01-03 07:53:37 +0800

    Update odd.c

--- odd.c
+++ odd.c
@@ -6,8 +6,7 @@ int main(){
     for(i=1; i<=100; ++i){
         rem = i % 2;
     if(rem != 0)
-        printf("\n  %d",i);
-               }
+        printf("\n  %d",i);}
     return 0; 
     
     } 
