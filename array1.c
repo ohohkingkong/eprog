294c8042d6c98053dc1f65822f665db62bb104c5
KING, JAN ERWIN S <janerwinking.jek@gmail.com>
2017-01-31 08:12:50 +0800

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int  main(){
+	int i, sum=0, average, grades[3];
+	for(i=0; i<3; i++){
+		printf("Enter grade %d:");
+		scanf("%d", &grades[i]);
+		
+		}
+		for (i=0; i<3; i++){
+			sum += grades[i];
+		}
+		average = sum/3;
+		printf("The 2nd grade is: %d\n", average);
+		if(average>=75){
+			printf("pass");
+		}else{
+			printf("fail");
+		}
+	
+}
