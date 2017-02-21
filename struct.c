b22d99693136377d9c544da446d2af57c4e0a642
Edwin Richbald A. Salinas <edsalinas7b@yahoo.com>
2017-02-21 09:23:34 +0800

    Reads file

--- struct.c
+++ struct.c
@@ -11,10 +11,39 @@ struct student{
 }; 
 
 int main (){
-   struct student stud;
-
-   strcpy(stud.name, "edos");
-   stud.info.id = 123456789 ;
-   stud.info.gpa = 3.4 ;
-   printf ("%s %ld %f\n", stud.name, stud.info.id, stud.info.gpa) ;
+	struct student stud;
+	FILE *myFile;
+    int choice = 1;
+    int num_records = 0;
+    
+    myFile = fopen("sample.txt", "a");
+	while(choice = 1){
+		printf("Enter student name: ");
+		scanf("%s", &stud.name);
+		printf("Enter student I.D.: ");
+		scanf("%d", &stud.info.id);
+		printf("Enter student GPA: ");
+		scanf("%f", &stud.info.gpa);
+		printf("%s %ld %.2f\n", stud.name, stud.info.id, stud.info.gpa);
+		fprintf(myFile, "%s %ld %.2f\n", stud.name, stud.info.id, stud.info.gpa);	
+		printf("Add another record?[1/0]: ");
+		scanf("%d", &choice);
+		if(choice == 0){
+			break;
+		}
+	}
+	fclose(myFile);
+	
+	myFile = fopen("sample.txt", "r");
+	//check if file exists
+    if(myFile == NULL){
+    	printf("Error: File does not exist");
+    	exit(1);
+	}
+	char temp[128];
+	while (fgets(temp, sizeof temp, myFile) != NULL) {
+		sscanf(temp, "%s %d %f", &stud.name, &stud.info.id, &stud.info.gpa);
+		printf("%s %d %f\n", stud.name, stud.info.id, stud.info.gpa);
+	}
+	fclose(myFile);
 } 
